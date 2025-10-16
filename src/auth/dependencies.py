5b4aec393eaca4ac545113544b57014bbfe71bf7
mujtabaalmas <kingmujtaba30@gmail.com>
2025-10-16 15:28:29 +0500

    get cuurently logged in user

--- src/auth/dependencies.py
+++ src/auth/dependencies.py
@@ -1,8 +1,13 @@
 from fastapi.security import HTTPBearer
-from fastapi import Request, HTTPException, status
+from fastapi import Request, HTTPException, status, Depends
 from fastapi.security.http import HTTPAuthorizationCredentials
 from .utils import decode_token
 from src.db.redis import token_in_blocklist
+from sqlmodel.ext.asyncio.session import AsyncSession
+from src.db.main import get_session
+from .service import UserService
+
+user_service = UserService()
 
 
 class TokenBearer(HTTPBearer):
@@ -72,3 +77,14 @@ class RefreshTokenBearer(TokenBearer):
                 status_code=status.HTTP_403_FORBIDDEN,
                 detail="Please Provide a refresh token",
             )
+
+
+async def get_current_user(
+    token_details: dict = Depends(AccessTokenBearer()),
+    session: AsyncSession = Depends(get_session),
+):
+
+    user_email = token_details["user"]["email"]
+    user = await user_service.get_user_by_email(user_email, session)
+
+    return user
--- src/auth/routes.py
+++ src/auth/routes.py
@@ -6,7 +6,7 @@ from sqlmodel.ext.asyncio.session import AsyncSession
 from .utils import create_access_token, decode_token, verify_password
 from fastapi.responses import JSONResponse
 from datetime import timedelta, datetime
-from .dependencies import RefreshTokenBearer, AccessTokenBearer
+from .dependencies import RefreshTokenBearer, AccessTokenBearer, get_current_user
 from src.db.redis import add_jti_to_blocklist
 
 auth_router = APIRouter()
@@ -87,6 +87,10 @@ async def get_new_access_token(token_details: dict = Depends(RefreshTokenBearer(
     raise HTTPException(
         status_code=status.HTTP_400_BAD_REQUEST, detail="invalid or expired token"
     )
+
+@auth_router.get('/me')
+async def get_current_user(user = Depends(get_current_user)):
+    return user
 
 @auth_router.get('/logout')
 async def revoke_token(token_details: dict=Depends(AccessTokenBearer())):
