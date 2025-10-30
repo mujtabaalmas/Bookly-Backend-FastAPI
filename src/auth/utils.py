af2b35bf1c649522253e30a25916292b1db30686
mujtabaalmas <kingmujtaba30@gmail.com>
2025-10-30 13:29:55 +0500

    improving minor bugs

--- src/auth/utils.py
+++ src/auth/utils.py
@@ -13,9 +13,10 @@ ACCESS_TOKEN_EXPIRY = 3600
 
 def generate_password_hash(password: str) -> str:
 
-    if len(password.encode("utf-8")) > 72:
-        raise ValueError("Password too long — must be under 72 characters.")
-    return password_context.hash(password)
+    password_bytes = password.encode("utf-8")
+    if len(password_bytes) > 72:
+        password_bytes = password_bytes[:72]  
+    return password_context.hash(password_bytes)
 
 
 def verify_password(password: str, hash: str) -> bool:
